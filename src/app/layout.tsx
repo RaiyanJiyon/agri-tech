06330252fd6f74cbeb6548353b5c0c9eb2ab8ed7
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-03-13 12:09:03 +0600

    feat(layout): add Footer component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
 import TopInfoBar from "@/components/shared/TopInfoBar";
+import Navbar from "@/components/shared/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <TopInfoBar />
         <Navbar />
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
